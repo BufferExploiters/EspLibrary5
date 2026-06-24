3d4be600143a949a78f541c5eb2a3e71ce77d559
BufferExploiters <florcairo08@gmail.com>
2026-06-24 18:01:03 +0800

    Update example.lua

--- esp/example.lua
+++ esp/example.lua
@@ -1,10 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BufferExploiters/EspLibrary5/refs/heads/main/esp/esp.lua"))()
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/BufferExploiters/EspLibrary5/refs/heads/main/esp/esp.lua"))()
 
-local Config = getgenv().Library.Table
+local Config = ESP.Table
 
 Config['Enabled'] = true
 Config['Distance'] = 500
 
+-- Boxes
 Config['Boxes']['Enabled'] = true
 Config['Boxes']['Gradients']['Top'] = Color3.fromRGB(0, 255, 255)
 Config['Boxes']['Gradients']['Bot'] = Color3.fromRGB(0, 255, 255)
@@ -24,13 +25,14 @@ Config['Boxes']['Filled']['Top'] = Color3.fromRGB(0, 255, 255)
 Config['Boxes']['Filled']['Bot'] = Color3.fromRGB(0, 255, 255)
 Config['Boxes']['Filled']['Transparency'] = {1, 0.75}
 
+-- Bars
 Config['Bars']['Health Bar']['Enabled'] = true
 Config['Bars']['Health Bar']['Top'] = Color3.fromRGB(0, 255, 0)
 Config['Bars']['Health Bar']['Mid'] = Color3.fromRGB(255, 255, 0)
 Config['Bars']['Health Bar']['Bot'] = Color3.fromRGB(255, 0, 0)
-
 Config['Bars']['Armor Bar']['Enabled'] = false
 
+-- Text
 Config['Texts']['Name']['Enabled'] = true
 Config['Texts']['Name']['Color'] = Color3.fromRGB(0, 255, 255)
 
@@ -39,3 +41,13 @@ Config['Texts']['Distance']['Color'] = Color3.fromRGB(0, 255, 255)
 
 Config['Texts']['Weapon']['Enabled'] = true
 Config['Texts']['Weapon']['Color'] = Color3.fromRGB(0, 255, 255)
+
+--unload example
+--[[ 
+	local InputService = game:GetService("UserInputService")
+
+	InputService.InputBegan:Connect(function(Input, Processed)
+		if not Processed and Input.KeyCode == Enum.KeyCode.Delete then
+			ESP:Unload()
+		end
+	end) ]]
